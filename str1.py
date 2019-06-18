7481c516e8c9b845d50f007d4b3c1053b4bfcd78
ramanuj760 <ramanujgupta760@gmail.com>
2019-06-18 20:53:57 +0530

    first

---
+++ str1.py
@@ -0,0 +1,19 @@
+a="hello this is a world"
+print(a.endswith("t"))
+b="python"
+print(b.startswith("y"))
+x='python      '
+y='course'
+z='*'.join([a,x,y])
+print(z)
+print(len(a))
+print(a.split("l"))
+ju=a.rjust(40,"*")
+lu=a.ljust(29,"/")
+print(ju)
+print(lu)
+p='iiiiiiiikliilrn'
+k=p.lstrip("i")
+print(k)
+j=b.rstrip()
+print(j)
--- string.py
+++ string.py
@@ -1,8 +1,32 @@
-#membership operator
-a="python learning"
-print(a)
-print(a[2])
-if "y" in a:
-    print("true y i ")
+a="hello i am using python "
+b="PYTHON"
+c="43896"
+print(type(a))
+print(len(a))
+if "he" in a:
+   print("true he in ")
 else:
-     print("false")
+   print("false")
+if type(a) is str:
+   print("yes a string value")
+else:
+   print("no a is not a string value")
+print(a.isalpha(),"alpha value") # it is true only when alpha value only
+print(b.isalpha(),"alpha value") # other wise false
+print(c.isdigit(),"digit value") #it shows only when number is string only true for digit
+print(a.isspace())
+print(a.islower(),"it is lowercase")
+print(b.isupper(),"it is uppercasr ")
+print(c.isalnum(),"it is alphanumeric value") # it is true for alphanumeric value or numeric
+print(a.split("l"))
+print(a.split())
+d=b.rjust(45,"*")
+print(d)
+e=c.ljust(55,"/")
+print(e)
+f="kkkkkpython"
+j=f.lstrip("k")
+print(j)
+o="ramaaaaaaaaaa"
+k=o.rstrip("a")
+print(k)
